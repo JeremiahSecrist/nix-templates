9cf83a3fd0390cf1e668ee81d86df8d3521d95a4
Jermeiah S <owner@arouzing.xyz>
2024-03-26 16:50:16 -0400

    fix lock and add example miri usage

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     crane = {
       url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -50,24 +50,109 @@
           overlays = [(import rust-overlay)];
         };
 
-        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-          targets = ["x86_64-unknown-linux-musl"];
+        rustToolchain = pkgs.rust-bin.selectLatestNightlyWith (toolchain:
+          toolchain.default.override {
+            targets = [
+              "x86_64-unknown-linux-musl"
+              # "x86_64-unknown-linux-gnu"
+            ];
+            extensions = [
+              "rust-src"
+              "rust-analyzer"
+              "rust-std"
+              "clippy"
+              "miri"
+            ];
+          });
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+        ci-run-miri-tests = pkgs.writeShellApplication {
+          name = "ci-run-miri-tests";
+          runtimeInputs =  with pkgs;[
+            gcc
+            stdenv
+            rustToolchain
+            glibc
+          ];
+          text = ''
+            pushd ./packages/hello_world
+            cargo miri clean
+            cargo miri setup
+            cargo miri run
+            popd
+          '';
+        };
+        ci-all = pkgs.writeShellApplication {
+          name = "ci-run-all";
+          runtimeInputs = [
+            ci-run-miri-tests
+          ];
+          text = ''
+            ci-run-miri-tests
+          '';
+        };
+        commonArgs = {
+          strictDeps = true;
+          # CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+          # CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+          buildInputs = [];
+          RUST_BACKTRACE = 1;
+        };
+        createPackageStruct = recipe: {
+          inherit recipe;
+          package = craneLib.buildPackage recipe;
+          cargoArtifacts = craneLib.buildDepsOnly recipe;
+          audit = craneLib.cargoAudit ({inherit advisory-db;} // recipe);
+          clippy = craneLib.cargoClippy recipe;
+          next-test = craneLib.cargoNextest recipe;
+          fmt = craneLib.cargoFmt recipe;
+          shell = craneLib.devShell {
+            checks = self.checks;
+            inputsFrom = [(craneLib.buildPackage recipe)];
+            RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+            CARGO_BUILD_TARGET = recipe.CARGO_BUILD_TARGET;
+            CARGO_BUILD_RUSTFLAGS = recipe.CARGO_BUILD_RUSTFLAGS;
+            packages = with pkgs; [
+              cargo-audit
+              cargo-bloat
+              cargo-deny
+              cargo-deps
+              cargo-diet
+              cargo-llvm-lines
+              cargo-msrv
+              cargo-rr
+              cargo-sort
+              cargo-vet
+              cargo-watch
+              bacon
+              rustup
+              rust-analyzer
+              pkg-config
+            ];
+          };
         };
 
-        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
-
-        hello-world = craneLib.buildPackage {
+        hello-world = createPackageStruct {
           src = craneLib.cleanCargoSource (craneLib.path ./packages/hello_world);
           strictDeps = true;
-
           CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
           CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+          inherit (hello-world) cargoArtifacts;
         };
       in {
         checks = {
-          inherit hello-world;
+          hello-world = hello-world.package;
+          hello-world-audit = hello-world.audit;
+          hello-world-nxt-test = hello-world.next-test;
+          hello-world-clippy = hello-world.clippy;
+          hello-world-fmt = hello-world.fmt;
         };
-
-        packages.default = hello-world;
+        devShells = {
+          default = hello-world.shell;
+        };
+        packages = {
+          default = hello-world.package;
+          inherit ci-all ci-run-miri-tests;
+        };
+        
       });
 }
